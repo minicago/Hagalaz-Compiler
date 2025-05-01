53bffa637ac8046a7aab7c153f32596cf84834a8
minicago <vipbyy@126.com>
2025-05-01 21:54:50 +0800

    add: SysyFunc

--- compiler/scope.cpp
+++ compiler/scope.cpp
@@ -6,9 +6,34 @@
 #include <map>
 #include <vector>
 
+void defineLibFunc(Scope& scope, std::string id, yytokentype returnType) {
+    auto funcDefNode = FuncDefNode(returnType, id, nullptr, nullptr);
+    auto func = std::make_shared<FuncDecl>(returnType, id, funcDefNode);
+    func->paramList = std::make_shared<std::vector<std::shared_ptr<VarDecl>>>();
+    scope.addFunc(id, func);
+}
 
+void addLibFuncArg(Scope& scope, std::string id, std::shared_ptr<SysyType> type, std::string argId) {
+    auto param = std::make_shared<VarDecl>(argId, TypeValue(type));
+    auto func = scope.findFunc(id);
+    if (func) {
+        func->paramList->push_back(param);
+    }
+}
 
-Scope::Scope() {}
+Scope::Scope() {
+    defineLibFunc(*this, "getint", INT);
+    defineLibFunc(*this, "getch", INT);
+    defineLibFunc(*this, "getarray", INT);
+        addLibFuncArg(*this, "getarray", std::make_shared<ArrayType>(std::make_shared<SimpleType>(SysyType::IntegerTyID)), "array");
+    defineLibFunc(*this, "putint", VOID);
+        addLibFuncArg(*this, "putint", std::make_shared<SimpleType>(SysyType::IntegerTyID), "value");
+    defineLibFunc(*this, "putch", VOID);
+        addLibFuncArg(*this, "putch", std::make_shared<SimpleType>(SysyType::IntegerTyID), "value");
+    defineLibFunc(*this, "putarray", VOID);
+        addLibFuncArg(*this, "putarray", std::make_shared<ArrayType>(std::make_shared<SimpleType>(SysyType::IntegerTyID)), "array");
+
+}
 
 Scope::~Scope() {}
 
